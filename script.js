98af81a8c6b2298cece2873ded1d9d5b75c6dbdb
Geri Kona <gerikona@hotmail.com>
2018-05-18 12:46:54 -0400

    Part 2

--- script.js
+++ script.js
@@ -27,3 +27,17 @@ var months;
 var today = new Date();
 var todayMsg = "Today is " + days[today.getDay()] + " " + months[today.getMonth()] + " " + today.getDate() + ", " + today.getFullYear() + ".";
 document.querySelector('#p--today-date').innerHTML = todayMsg;
+//PART 2
+var btn = document.querySelector("#button--birthday");
+btn.addEventListener("click", function () {
+    document.querySelector('#p--birthday-message').innerHTML = mybday(new Date(document.querySelector('#input--date-picker').value));
+});
+function mybday(bday) {
+    var bdayMsg = "Your birthday will be on " + days[bday.getDay()] + " " + months[bday.getMonth()] + " " + bday.getDate() + ", " + bday.getFullYear() + ".";
+    var todayCheck = months[today.getMonth()] + " " + today.getDate() + ", " + today.getFullYear();
+    var bdayCheck = months[bday.getMonth()] + " " + bday.getDate() + ", " + bday.getFullYear();
+    if (bdayCheck == todayCheck) {
+        bdayMsg = "HappyBirthday!";
+    }
+    return bdayMsg;
+}
